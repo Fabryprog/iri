9fbe45fc364e69c2b82e32fd647031ca727961ce
Fabrizio Spataro fabryprog <fabryprog@gmail.com>
2018-11-18 08:06:21 +0100

    added log

--- src/main/java/com/iota/iri/dpow/HazelcastInstanceReconnectorTask.java
+++ src/main/java/com/iota/iri/dpow/HazelcastInstanceReconnectorTask.java
@@ -1,10 +1,14 @@
 package com.iota.iri.dpow;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.Member;
 
 public class HazelcastInstanceReconnectorTask implements Runnable {
-	
+    private static final Logger log = LoggerFactory.getLogger(HazelcastInstanceReconnectorTask.class);
+
 	private com.hazelcast.config.Config config = null;
 	
 	public HazelcastInstanceReconnectorTask(com.hazelcast.config.Config config) {
@@ -12,6 +16,8 @@ public class HazelcastInstanceReconnectorTask implements Runnable {
 	}
 	
 	private void restart() {
+		log.info("<< Cluster Restarting >>>");
+		
 		Hazelcast.getHazelcastInstanceByName("IRI").shutdown();
 		
 		Hazelcast.newHazelcastInstance(config);
@@ -22,6 +28,8 @@ public class HazelcastInstanceReconnectorTask implements Runnable {
 		try {
 			while (true) {
 				Thread.currentThread().sleep(30000);
+				
+				log.info("Checking Cluster status [Cluster members: " + Hazelcast.getHazelcastInstanceByName("IRI").getCluster().getMembers().size() + " ]");
 				
 				boolean connectedToMainMember = false; 
 				for(Member m : Hazelcast.getHazelcastInstanceByName("IRI").getCluster().getMembers()) {
